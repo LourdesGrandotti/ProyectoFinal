ffd19a9ec7a59c9fc00507173beaf92ca9e8a065
JohannaMiranda <mirandajohanna88@gmail.com>
2025-12-06 01:35:46 -0300

    se crea metodo "Destino"

--- turismo_chaco/blogTurismo/models.py
+++ turismo_chaco/blogTurismo/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Destino(models.Model):
+    id_destino=models.BigAutoField(primary_key=True)
+    nombre = models.CharField(max_Length=200)
+    descripcion = models.TextField()
+    ubicacion = models.CharField(max_length=200)
+    categoria = models.CharField(
+        max_length=50,
+        choices=[
+            ("Naturaleza", "Naturaleza"),
+            ("Cultura", "Cultura"),
+            ("Gastronomia", "Gastronomia"),
+        ]
+    )
+
+    def __str__(self):
+        return self.nombre
