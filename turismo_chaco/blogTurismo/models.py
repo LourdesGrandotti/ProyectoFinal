3bf41773e124ee9b9328c357f9853f41c6ccf1e5
JohannaMiranda <mirandajohanna88@gmail.com>
2025-12-06 02:09:41 -0300

    se crea modelo para "Evento"

--- turismo_chaco/blogTurismo/models.py
+++ turismo_chaco/blogTurismo/models.py
@@ -17,3 +17,14 @@ class Destino(models.Model):
 
     def __str__(self):
         return self.nombre
+
+class Evento(models.Model):
+    id_evento=models.BigAutoField(primary_key=True)
+    titulo = models.CharField(max_Length=200)
+    fecha = models.DateField()
+    lugar = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    imagen = models.ImageField(upload_to="eventos/", blank=True, null=True)
+
+def __str_(self):
+    return F"{self.titulo} - {self.fecha}"
